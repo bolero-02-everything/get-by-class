ce57f245a6939fdce906242622200d27d4647d3b
genta <bolero.02.everything@gmail.com>
2014-07-05 02:44:20 +0900

    use jQuery for IE8 compatibility

--- index.js
+++ index.js
@@ -10,44 +10,51 @@
  */
 
 module.exports = (function() {
-  if (document.getElementsByClassName) {
-    return function(container, className, single) {
-      if (single) {
-        return container.getElementsByClassName(className)[0];
-      } else {
-        return container.getElementsByClassName(className);
-      }
-    };
-  } else if (document.querySelector) {
-    return function(container, className, single) {
-      className = '.' + className;
-      if (single) {
-        return container.querySelector(className);
-      } else {
-        return container.querySelectorAll(className);
-      }
-    };
-  } else {
-    return function(container, className, single) {
-      var classElements = [],
-        tag = '*';
-      if (container == null) {
-        container = document;
-      }
-      var els = container.getElementsByTagName(tag);
-      var elsLen = els.length;
-      var pattern = new RegExp("(^|\\s)"+className+"(\\s|$)");
-      for (var i = 0, j = 0; i < elsLen; i++) {
-        if ( pattern.test(els[i].className) ) {
-          if (single) {
-            return els[i];
-          } else {
-            classElements[j] = els[i];
-            j++;
-          }
-        }
-      }
-      return classElements;
-    };
-  }
+
+	var $ = jQuery;
+	return function(container, clazz, single) {
+		var ret = $(container).find('.' + clazz).get();
+		return single ? ret[0] : ret;
+	};
+
+//  if (document.getElementsByClassName) {
+//    return function(container, className, single) {
+//      if (single) {
+//        return container.getElementsByClassName(className)[0];
+//      } else {
+//        return container.getElementsByClassName(className);
+//      }
+//    };
+//  } else if (document.querySelector) {
+//    return function(container, className, single) {
+//      className = '.' + className;
+//      if (single) {
+//        return container.querySelector(className);
+//      } else {
+//        return container.querySelectorAll(className);
+//      }
+//    };
+//  } else {
+//    return function(container, className, single) {
+//      var classElements = [],
+//        tag = '*';
+//      if (container == null) {
+//        container = document;
+//      }
+//      var els = container.getElementsByTagName(tag);
+//      var elsLen = els.length;
+//      var pattern = new RegExp("(^|\\s)"+className+"(\\s|$)");
+//      for (var i = 0, j = 0; i < elsLen; i++) {
+//        if ( pattern.test(els[i].className) ) {
+//          if (single) {
+//            return els[i];
+//          } else {
+//            classElements[j] = els[i];
+//            j++;
+//          }
+//        }
+//      }
+//      return classElements;
+//    };
+//  }
 })();
